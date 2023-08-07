4c61e51b5ac771bb270f3a62cd945444c4f32426
Marcos Francesquini <francesquini.personal@gmail.com>
2023-08-07 22:24:06 +0200

    adjust details about a subject

--- routes/subjects.routes.js
+++ routes/subjects.routes.js
@@ -4,13 +4,25 @@ const Book = require('../models/Book.model');
 const router = express.Router();
 
 router.get("/new-subject", (req, res) => {
-    res.render("new-subject")
+    Book.find()
+        .then((allBooks) => {
+            res.render('new-subject', { allBooks })
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 router.post("/new-subject", (req, res) => {
     const { name, pricePerHour } = req.body
-    Subject.create({ name, pricePerHour })
+    const books = req.body.book
+
+    Subject.create({ name, pricePerHour, books })
         .then((newSubject) => {
+            return Book.findByIdAndUpdate(newSubject.books, { $push: { subject: newSubject._id } })
+        })
+        .then((updatedBook) => {
             res.redirect("/subject")
         })
         .catch((err) => {
@@ -18,17 +30,90 @@ router.post("/new-subject", (req, res) => {
         })
 })
 
-router.get("/:id/details", (req, res, next) => {
-    console.log(req.params.id)
+/*router.get("/:id/details", (req, res, next) => {
     Subject.findById(req.params.id)
         .then((subjectToBeShowed) => {
-            res.render("show-subject",  subjectToBeShowed);
+            return subjectToBeShowed
+        })
+        .then((oneSubject) => {
+            const arrayBooks = oneSubject.books
+            let thinyArray = []
+
+            arrayBooks.forEach(item => {
+                Book.findById(item)
+                    .then((bookDetails) => {
+                        console.log("------------bookDetails.name---------------")                        
+                        console.log(bookDetails.name)
+                        thinyArray = { "name": bookDetails.name, "price": bookDetails.price }
+                    })
+
+                    .catch((err) => {
+                        console.log(err)
+                    })
+
+            });
+            console.log("-------------thinyArray--------------")
+            console.log(thinyArray)
+            res.render("show-subject", oneSubject);
         })
         .catch((err) => {
             console.log(err)
         })
+});*/
+
+router.get("/:id/details", (req, res, next) => {
+    Subject.findById(req.params.id)
+        .then(subjectToBeShowed => {
+            const arrayBooks = subjectToBeShowed.books;
+            const thinyArray = [];
+
+            const bookPromises = [];
+
+            for (const item of arrayBooks) {
+                bookPromises.push(
+                    Book.findById(item)
+                        .then(bookDetails => {
+                            return { "name": bookDetails.name, "price": bookDetails.price };
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
+                );
+            }
+
+            Promise.all(bookPromises)
+                .then(results => {
+                    thinyArray.push(...results);
+
+                    console.log("-------------thinyArray--------------");
+                    console.log(thinyArray);
+
+                    res.render("show-subject", {
+                        subject: subjectToBeShowed,
+                        booksArray: thinyArray 
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    // Tratar erro adequadamente
+                });
+        })
+        .catch(err => {
+            console.log(err);
+            // Tratar erro adequadamente
+        });
 });
 
+router.post('/:id/delete', (req, res) => {
+    console.log(req.params)
+    Subject.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/subject')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
 
 router.get("/", (req, res, next) => {
     Subject.find()
