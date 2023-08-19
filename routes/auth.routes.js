8a0327d01c320028ec44756b55fc63f1e3974396
Felipe Mantovani <92172133+f-mantovani@users.noreply.github.com>
2023-08-19 11:26:55 +0200

    minor(auth): added admin to the auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -32,7 +32,7 @@ router.post("/signup", (req, res) => {
 
                     let Mymodel;
 
-                    if (type === "Teacher") {
+                    if (type === "Teacher" || type === "Admin") {
                         Mymodel = require('../models/Teacher.model');
                     } else {
                         Mymodel = require('../models/Student.model');
