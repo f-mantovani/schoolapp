058869f74dcdb402672ea6aeb6908ed8cceb046f
Marcos Francesquini <francesquini.personal@gmail.com>
2023-08-19 10:45:41 +0200

    configuring

--- config/session.config.js
+++ config/session.config.js
@@ -1,8 +1,12 @@
 const session = require('express-session');
- 
+const MongoStore = require("connect-mongo");
+require("dotenv").config() 
 // since we are going to USE this middleware in the app.js,
 // let's export it and have it receive a parameter
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/schoolapp";
 module.exports = app => {
+
   // <== app is just a placeholder here
   // but will become a real "app" in the app.js
   // when this file gets imported/required there
@@ -10,15 +14,12 @@ module.exports = app => {
   // use session
   app.use(
     session({
-      secret: process.env.SESS_SECRET,
-      resave: true,
+      secret: process.env.SESS_SECRET || "super hyper secret key",
+      resave: false,
       saveUninitialized: false,
-      cookie: {
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true,
-        maxAge: 60000*120 // 60 * 1000 ms === 1 min
-      }
+      store: MongoStore.create({
+        mongoUrl: MONGO_URI,
+      }),
     })
   );
 };
