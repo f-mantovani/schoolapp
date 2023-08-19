c7134f8aff742ca22ed08446143547dba54054eb
Marcos Francesquini <francesquini.personal@gmail.com>
2023-08-19 10:32:29 +0200

    access

--- config/session.config.js
+++ config/session.config.js
@@ -17,7 +17,7 @@ module.exports = app => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000*30 // 60 * 1000 ms === 1 min
+        maxAge: 60000*120 // 60 * 1000 ms === 1 min
       }
     })
   );
